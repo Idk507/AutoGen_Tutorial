0d26c34462935ef79a9428b67453ab1c3f9aa7fc
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2024-12-09 20:07:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import autogen
+
+config_list = [
+    
+        {
+        "model": "gemini-pro",
+        "api_key": "AIzaSyCvDhMJK9JCVev9GSrgOSh-_GgwEqzOExU",
+        "api_type": "google"
+    }
+    
+]
